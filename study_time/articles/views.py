0c699afcad5b89272cb0e94265efd77ef86f42cf
natalia-sk <nataliaszokaluk@gmail.com>
2021-08-05 18:13:57 +0200

    create first view of articles + first migration

--- study_time/articles/views.py
+++ study_time/articles/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Article
+from .serializers import ArticleSerializer
+
+@api_view(["GET"])
+def list_articles(request):
+    articles = Article.objects.all()
+    serializers = ArticleSerializer(articles, many=True)
+    content = {
+        "articles": serializers.data,
+    }
+    return Response(content)
