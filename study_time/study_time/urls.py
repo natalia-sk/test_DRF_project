86bef8dc6aa1cfe216878b370f3aa0ef830a109c
natalia-sk <nataliaszokaluk@gmail.com>
2021-08-23 10:25:11 +0200

    create model, view, serializer, urls + migration

--- study_time/study_time/urls.py
+++ study_time/study_time/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include("articles.urls")),
     path('courses/', include("courses.urls")),
+    path('notifications/', include("notifications.urls"))
 ]
