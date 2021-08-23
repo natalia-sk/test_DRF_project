d415f113b2cb56006897885fde3f0e9085f3cca8
natalia-sk <nataliaszokaluk@gmail.com>
2021-08-23 15:52:09 +0200

    add filtering in courses app

--- study_time/courses/views.py
+++ study_time/courses/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Course, Episode
 from .serializers import CourseSerializer, EpisodeSerializer
@@ -7,8 +8,13 @@ from .serializers import CourseSerializer, EpisodeSerializer
 class EpisodeViewSet(viewsets.ModelViewSet):
     serializer_class = EpisodeSerializer
     queryset = Episode.objects.all()
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['title']
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['language']
+    ordering_fields = ['title']
