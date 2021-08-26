4ffb319c6947eb0388c5eaf1ebd852b15d443c20
natalia-sk <nataliaszokaluk@gmail.com>
2021-08-26 10:07:20 +0200

    remove "OrderingFilter" from "filter_backends".

--- study_time/courses/views.py
+++ study_time/courses/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Course, Episode
@@ -8,13 +8,10 @@ from .serializers import CourseSerializer, EpisodeSerializer
 class EpisodeViewSet(viewsets.ModelViewSet):
     serializer_class = EpisodeSerializer
     queryset = Episode.objects.all()
-    filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['title']
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend]
     filterset_fields = ['language']
-    ordering_fields = ['title']
