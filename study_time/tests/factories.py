17abfd1e949170d72553f7ba60358893ca5b3235
natalia-sk <nataliaszokaluk@gmail.com>
2021-09-09 14:20:21 +0200

    Create 'UserFactory', 'user_fixture' and forcing user authentication.

---
+++ study_time/tests/factories.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from factory import django
+
+
+class UserFactory(django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = "test_user"
+    password = "test_password"
