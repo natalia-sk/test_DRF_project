ce3ac522ebc8eb9052b3a2de01ff0dc6449b01ff
natalia-sk <nataliaszokaluk@gmail.com>
2021-09-10 14:17:47 +0200

    Test for article serializer

--- study_time/articles/tests/tests.py
+++ study_time/articles/tests/tests.py
@@ -1,6 +1,7 @@
 import pytest
 
 from .factories import ArticleFactory
+from articles.serializers import ArticleSerializer
 from tests.utils import get_client
 
 
@@ -14,3 +15,12 @@ def test_article(user_fixture):
     # THEN
     assert request.status_code == 200
     assert request.data == {"id": article.id, "title": article.title, "content": article.content}
+
+
+def test_serializer_contains_expected_fields():
+    # GIVEN
+    serializer = ArticleSerializer()
+    # WHEN
+    serializer_fields = serializer.fields
+    # THEN
+    assert set(serializer_fields.keys()) == set(['id', 'title', 'content'])
