04a757c8e09277e565078946dae36cfed6a00caa
natalia-sk <nataliaszokaluk@gmail.com>
2021-09-10 16:08:54 +0200

    Change database, add sensitive data to .gitignore file

--- study_time/study_time/settings.py
+++ study_time/study_time/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from .database_settings import postgres_settings
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,12 +80,7 @@ WSGI_APPLICATION = 'study_time.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = postgres_settings
 
 
 # Password validation
