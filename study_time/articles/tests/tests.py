9963c4f75ea118f9a5ea43b0858644cf99d3e152
natalia-sk <nataliaszokaluk@gmail.com>
2021-09-14 11:35:37 +0200

    Tests for article view serializer class and permission classes

--- study_time/articles/tests/tests.py
+++ study_time/articles/tests/tests.py
@@ -1,12 +1,13 @@
-from articles.views import ArticleViewSet
 import pytest
 import re
 from django.db.utils import DataError
 from rest_framework.test import APIClient
+from rest_framework.permissions import IsAuthenticated
 
 from .factories import ArticleFactory
 from articles.models import Article
 from articles.serializers import ArticleSerializer
+from articles.views import ArticleViewSet
 from tests.utils import get_client
 
 
@@ -47,3 +48,11 @@ def test_article_model_raises_exception():
     with pytest.raises(DataError, match=re.escape("value too long for type character varying(150)")):
         # WHEN
         Article.objects.create(title=f"{'test' * 150}", content="test content")
+
+
+def test_article_view_serialzier_class():
+    assert ArticleViewSet.serializer_class == ArticleSerializer
+
+
+def test_article_view_permission_classes():
+    assert ArticleViewSet.permission_classes == [IsAuthenticated]
