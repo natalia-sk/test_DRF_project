0a3c519e1406c8414452eae0469f0982dd8ed499
natalia-sk <nataliaszokaluk@gmail.com>
2021-09-15 17:09:44 +0200

    Fixes in test_article_view.py

--- study_time/articles/tests/test_article_view.py
+++ study_time/articles/tests/test_article_view.py
@@ -1,5 +1,8 @@
 import pytest
 
+from django.urls import reverse
+
+from rest_framework import status
 from rest_framework.test import APIClient
 from rest_framework.permissions import IsAuthenticated
 
@@ -11,24 +14,30 @@ from tests.utils import get_client
 
 @pytest.mark.django_db
 def test_authenticated_user_can_access_article_details(user_fixture):
+
     # GIVEN
     article = ArticleFactory()
     client = get_client(user_fixture)
+
     # WHEN
-    request = client.get('/articles/articles/1')
+    request = client.get(reverse("articles-detail", kwargs={"pk": article.pk}))
+
     # THEN
-    assert request.status_code == 200
+    assert request.status_code == status.HTTP_200_OK
     assert request.data == {"id": article.id, "title": article.title, "content": article.content}
 
 
 @pytest.mark.django_db
 def test_unauthenticated_user_cannot_access_articles_view(user_fixture):
+
     # GIVEN
     client = APIClient(user_fixture)
+
     # WHEN
-    request = client.get('/articles/articles')
+    request = client.get(reverse("articles-list"))
+    
     # THEN
-    assert request.status_code == 403
+    assert request.status_code == status.HTTP_403_FORBIDDEN
 
 
 def test_article_view_serialzier_class():
