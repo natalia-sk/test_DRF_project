5aaeb802b86af171f2560d712783faf644e2b686
natalia-sk <nataliaszokaluk@gmail.com>
2021-09-16 11:56:33 +0200

    Fixes in test_article_serializer.py

--- study_time/articles/tests/test_article_serializer.py
+++ study_time/articles/tests/test_article_serializer.py
@@ -2,9 +2,12 @@ from articles.serializers import ArticleSerializer
 
 
 def test_serializer_contains_expected_fields():
+
     # GIVEN
     serializer = ArticleSerializer()
+
     # WHEN
     serializer_fields = serializer.fields
+
     # THEN
-    assert set(serializer_fields.keys()) == set(['id', 'title', 'content'])
+    assert set(serializer_fields.keys()) == set(["id", "title", "content"])
