f0d1f69d69787739bddf02dd26f37c30432ca69a
natalia-sk <nataliaszokaluk@gmail.com>
2021-09-16 17:05:37 +0200

    Additional tests in test_article_view.py

--- study_time/articles/tests/test_article_view.py
+++ study_time/articles/tests/test_article_view.py
@@ -7,9 +7,18 @@ from rest_framework.test import APIClient
 from rest_framework.permissions import IsAuthenticated
 
 from .factories import ArticleFactory
+from articles.models import Article
 from articles.serializers import ArticleSerializer
 from articles.views import ArticleViewSet
 from tests.utils import get_client
+
+
+def test_article_view_serialzier_class():
+    assert ArticleViewSet.serializer_class == ArticleSerializer
+
+
+def test_article_view_permission_classes():
+    assert ArticleViewSet.permission_classes == [IsAuthenticated]
 
 
 @pytest.mark.django_db
@@ -35,14 +44,81 @@ def test_unauthenticated_user_cannot_access_articles_view(user_fixture):
 
     # WHEN
     request = client.get(reverse("articles-list"))
-    
+
     # THEN
     assert request.status_code == status.HTTP_403_FORBIDDEN
 
 
-def test_article_view_serialzier_class():
-    assert ArticleViewSet.serializer_class == ArticleSerializer
+@pytest.mark.django_db
+def test_create_article(user_fixture):
+
+    # GIVEN
+    client = get_client(user_fixture)
+    data = {
+        "title": "test article title",
+        "content": "test article content"
+    }
+
+    # WHEN
+    response = client.post(path=reverse("articles-list"), data=data, format="json")
+
+    # THEN
+    assert response.status_code == status.HTTP_201_CREATED
+    assert Article.objects.get().id == response.json()["id"]
+    assert Article.objects.get().title == response.json()["title"]
+    assert Article.objects.get().content == response.json()["content"]
 
 
-def test_article_view_permission_classes():
-    assert ArticleViewSet.permission_classes == [IsAuthenticated]
+@pytest.mark.django_db
+def test_list_articles(user_fixture):
+
+    # GIVEN
+    client = get_client(user_fixture)
+    ArticleFactory.create_batch(3)
+
+    # WHEN
+    response = client.get(reverse("articles-list"))
+
+    # THEN
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.json()["results"]) == 3
+
+
+@pytest.mark.django_db
+def test_update_articles(user_fixture):
+
+    # GIVEN
+    client = get_client(user_fixture)
+    article = ArticleFactory()
+    new_data = {
+        "title": "test new article title",
+        "content": "test new article content"
+    }
+
+    # WHEN
+    response = client.put(
+        path=reverse("articles-detail", kwargs={"pk": article.id}), 
+        data=new_data, 
+        format="json"
+        )
+
+    # THEN
+    assert response.status_code == status.HTTP_200_OK
+    assert Article.objects.get().id == response.json()["id"]
+    assert Article.objects.get().title == response.json()["title"]
+    assert Article.objects.get().content == response.json()["content"]
+
+
+@pytest.mark.django_db
+def test_delete_articles(user_fixture):
+
+    # GIVEN
+    client = get_client(user_fixture)
+    article = ArticleFactory()
+
+    # WHEN
+    response = client.delete(path=reverse("articles-detail", kwargs={"pk": article.id}))
+
+    # THEN
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert Article.objects.all().count() == 0
