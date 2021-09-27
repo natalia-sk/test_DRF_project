9b26a73d34e0deccc59ecb9d3e87e372559ac2fe
natalia-sk <nataliaszokaluk@gmail.com>
2021-09-27 16:13:42 +0200

    Fixes in test_article_view.py

--- study_time/articles/tests/test_article_view.py
+++ study_time/articles/tests/test_article_view.py
@@ -6,9 +6,9 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from rest_framework.permissions import IsAuthenticated
 
-from .factories import ArticleFactory
 from articles.models import Article
 from articles.serializers import ArticleSerializer
+from articles.tests import values
 from articles.views import ArticleViewSet
 from tests.utils import get_client
 
@@ -22,14 +22,14 @@ def test_article_view_permission_classes():
 
 
 @pytest.mark.django_db
-def test_authenticated_user_can_access_article_details(user_fixture):
+def test_authenticated_user_can_access_article_details(user_fixture, article_fixture):
 
     # GIVEN
-    article = ArticleFactory()
+    article = Article.objects.get()
     client = get_client(user_fixture)
 
     # WHEN
-    request = client.get(reverse("articles-detail", kwargs={"pk": article.pk}))
+    request = client.get(reverse("articles-detail", kwargs={"pk": values.ARTICLE_ID}))
 
     # THEN
     assert request.status_code == status.HTTP_200_OK
@@ -61,35 +61,40 @@ def test_create_article(user_fixture):
 
     # WHEN
     response = client.post(path=reverse("articles-list"), data=data, format="json")
+    response_data = response.json()
+    article = Article.objects.get()
+    expected_data = {
+        "id": article.id, 
+        "title": article.title, 
+        "content": article.content
+        }
 
     # THEN
     assert response.status_code == status.HTTP_201_CREATED
-    assert Article.objects.get().id == response.json()["id"]
-    assert Article.objects.get().title == response.json()["title"]
-    assert Article.objects.get().content == response.json()["content"]
+    assert response_data == expected_data
 
 
 @pytest.mark.django_db
-def test_list_articles(user_fixture):
+def test_list_articles(user_fixture, three_articles_fixture):
 
     # GIVEN
     client = get_client(user_fixture)
-    ArticleFactory.create_batch(3)
 
     # WHEN
     response = client.get(reverse("articles-list"))
+    response_data = response.json()
 
     # THEN
     assert response.status_code == status.HTTP_200_OK
-    assert len(response.json()["results"]) == 3
+    assert response_data == values.ARTICLES_RESPONSE
 
 
 @pytest.mark.django_db
-def test_update_articles(user_fixture):
+def test_update_articles(user_fixture, article_fixture):
 
     # GIVEN
     client = get_client(user_fixture)
-    article = ArticleFactory()
+    article = article_fixture
     new_data = {
         "title": "test new article title",
         "content": "test new article content"
@@ -97,27 +102,32 @@ def test_update_articles(user_fixture):
 
     # WHEN
     response = client.put(
-        path=reverse("articles-detail", kwargs={"pk": article.id}), 
+        path=reverse("articles-detail", kwargs={"pk": values.ARTICLE_ID}), 
         data=new_data, 
         format="json"
         )
+    response_data = response.json()
+    
+    changed_article = Article.objects.get()
+    expected_changed_data = {
+        "id": changed_article.id, 
+        "title": changed_article.title, 
+        "content": changed_article.content
+        }
 
     # THEN
     assert response.status_code == status.HTTP_200_OK
-    assert Article.objects.get().id == response.json()["id"]
-    assert Article.objects.get().title == response.json()["title"]
-    assert Article.objects.get().content == response.json()["content"]
+    assert response_data == expected_changed_data
 
 
 @pytest.mark.django_db
-def test_delete_articles(user_fixture):
+def test_delete_articles(user_fixture, article_fixture):
 
     # GIVEN
     client = get_client(user_fixture)
-    article = ArticleFactory()
 
     # WHEN
-    response = client.delete(path=reverse("articles-detail", kwargs={"pk": article.id}))
+    response = client.delete(path=reverse("articles-detail", kwargs={"pk": values.ARTICLE_ID}))
 
     # THEN
     assert response.status_code == status.HTTP_204_NO_CONTENT
