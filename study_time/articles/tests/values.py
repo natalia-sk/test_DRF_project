3803be946a5d8169a60a8d83dd085860c4eb3da8
natalia-sk <nataliaszokaluk@gmail.com>
2021-09-28 12:07:11 +0200

    Fixes

--- study_time/articles/tests/values.py
+++ study_time/articles/tests/values.py
@@ -1,16 +1,34 @@
+from django.urls import reverse
+
+
 ARTICLE_ID = 1
 
 ARTICLES_IDS = [i for i in range(1,4)]
 ARTICLE_TITLE = "Some title, only for tests."
 ARTICLE_CONTENT = "A little longer text, only for tests"
 
+ARTICLE_DETAIL_PATH = reverse("articles-detail", kwargs={"pk": ARTICLE_ID})
+ARTICLES_LIST_PATH = reverse("articles-list")
+
 ARTICLES_RESPONSE = {
     "count": 3,
     "next": None, 
     "previous": None, 
     "results": [
-        {"id": ARTICLES_IDS[0], "title": ARTICLE_TITLE, "content": ARTICLE_CONTENT}, 
-        {"id": ARTICLES_IDS[1], "title": ARTICLE_TITLE, "content": ARTICLE_CONTENT}, 
-        {"id": ARTICLES_IDS[2], "title": ARTICLE_TITLE, "content": ARTICLE_CONTENT}
+        {"id": article_id, "title": ARTICLE_TITLE, "content": ARTICLE_CONTENT} for article_id in ARTICLES_IDS
         ]
     }
+
+DATA_NEW_ARTICLE = {
+    "title": "test article title", 
+    "content": "test article content"
+    }
+
+NEW_ARTICLE_RESPONSE = {"id": ARTICLE_ID, **DATA_NEW_ARTICLE}
+
+DATA_CHANGED_ARTICLE = {
+    "title": "test new article title", 
+    "content": "test new article content"
+    }
+
+ARTICLE_UPDATE_RESPONSE = {"id": ARTICLE_ID, **DATA_CHANGED_ARTICLE}
