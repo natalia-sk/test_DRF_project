ed20757ff8df96d17f82a0ff6dfe9e0a320b3a0c
natalia-sk <nataliaszokaluk@gmail.com>
2021-09-29 15:09:23 +0200

    Update 'fixtures.py' and 'values.py'

--- study_time/courses/tests/fixtures.py
+++ study_time/courses/tests/fixtures.py
@@ -7,3 +7,14 @@ from .factories import CourseFactory
 @pytest.fixture
 def course_fixture():
     return CourseFactory(id=values.COURSE_ID)
+
+
+@pytest.fixture
+def three_courses_fixture():
+    text_fields = {
+        "title": values.COURSE_TITLE,
+        "video_duration": values.COURSE_VIDEO_DURATION,
+        "language": values.COURSE_LANGUAGE
+        }
+    courses = [CourseFactory(id=id, **text_fields) for id in values.COURSES_IDS]
+    return courses
