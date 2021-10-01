0053da1115ef322a6d59b2b3ea5633c300952a95
natalia-sk <nataliaszokaluk@gmail.com>
2021-10-01 14:54:25 +0200

    Rename files

--- study_time/courses/tests/test_course_view.py
+++ study_time/courses/tests/test_course_view.py
@@ -4,10 +4,11 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.test import APIClient
 
-from courses.models import Course, Episode
-from courses.serializers import CourseSerializer, EpisodeSerializer
+from courses.models import Course
+from courses.serializers import CourseSerializer
 from courses.tests import values
-from courses.views import CourseViewSet, EpisodeViewSet
+from courses.views import CourseViewSet
+
 from tests.utils import get_client
 
 
@@ -115,63 +116,3 @@ def test_delete_course(user_fixture, course_fixture):
     # THEN
     assert response.status_code == status.HTTP_204_NO_CONTENT
     assert Course.objects.all().count() == 0
-
-
-def test_episode_view_serialzier_class():
-    assert EpisodeViewSet.serializer_class == EpisodeSerializer
-
-
-def test_episode_view_permission_classes():
-    assert EpisodeViewSet.permission_classes == [IsAuthenticated]
-    
-
-@pytest.mark.django_db
-def test_episode_detail(user_fixture, episode_fixture):
-
-    # GIVEN
-    episode = Episode.objects.get()
-    client = get_client(user_fixture)
-    data_check = {
-        "url": f"http://testserver/courses/episodes/{episode.id}", 
-        "id": episode.id,
-        "title": episode.title,
-        "video_url": episode.video_url,
-        "course": episode.course.id
-    }
-
-    # WHEN
-    request = client.get(values.EPISODE_DETAIL_PATH)
-
-    # THEN
-    assert request.status_code == status.HTTP_200_OK
-    assert request.data == data_check
-
-
-@pytest.mark.django_db
-def test_unauthenticated_user_cannot_access_episodes_view(user_fixture):
-
-    # GIVEN
-    client = APIClient(user_fixture)
-
-    # WHEN
-    request = client.get(values.EPISODES_LIST_PATH)
-
-    # THEN
-    assert request.status_code == status.HTTP_403_FORBIDDEN
-
-
-# TODO
-def test_create_episode():
-    pass
-
-
-def test_list_episodes():
-    pass
-
-
-def test_update_episode():
-    pass
-
-
-def test_delete_episode():
-    pass
