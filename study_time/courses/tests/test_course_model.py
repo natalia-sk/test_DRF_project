81482ed281c92821951240ab3fe4a15575e96baa
natalia-sk <nataliaszokaluk@gmail.com>
2021-10-04 16:32:51 +0200

    Additional tests for courses

--- study_time/courses/tests/test_course_model.py
+++ study_time/courses/tests/test_course_model.py
@@ -1,8 +1,9 @@
 import pytest
 import re
 
-from django.db.utils import DataError
+from django.db.utils import DataError, IntegrityError
 from courses.models import Course
+from courses.tests import values
 
 
 @pytest.mark.django_db
@@ -16,3 +17,26 @@ def test_course_title_max_length():
 
         # WHEN
         Course.objects.create(title="s" * 151, video_duration=123, language="en")
+
+
+@pytest.mark.django_db()
+def test_course_language_default_value():
+
+    # WHEN
+    course = Course.objects.create(title=values.COURSE_TITLE, video_duration=values.COURSE_VIDEO_DURATION)
+
+    # THEN
+    assert course.language == "en"
+
+
+@pytest.mark.django_db
+def test_course_video_duration_not_null():
+    
+    # THEN
+    with pytest.raises(
+        IntegrityError, 
+        match='null value in column "video_duration" violates not-null constraint'
+        ):
+
+        # WHEN
+        Course.objects.create()
