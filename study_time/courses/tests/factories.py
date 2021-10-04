379e74152989fba292e9bea7d0c6d6881743c06e
natalia-sk <nataliaszokaluk@gmail.com>
2021-10-04 15:25:37 +0200

    Small changes

--- study_time/courses/tests/factories.py
+++ study_time/courses/tests/factories.py
@@ -7,7 +7,7 @@ class CourseFactory(django.DjangoModelFactory):
 
     title = Faker("sentence", nb_words=3)
     video_duration = Faker("random_int", min=1, max=999, step=1)
-    language = Faker("random_element", elements=('en', 'ru', 'de', 'fr'))
+    language = Faker("random_element", elements=("en", "ru", "de", "fr"))
 
 
 class EpisodeFactory(django.DjangoModelFactory):
