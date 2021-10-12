d19832bc07ca5eda638846387d99015bfa9c8e84
natalia-sk <nataliaszokaluk@gmail.com>
2021-10-12 10:17:19 +0200

    Change length of titles

--- study_time/notifications/signals.py
+++ study_time/notifications/signals.py
@@ -4,29 +4,35 @@ from .models import Notification
 from articles.models import Article
 from courses.models import Course, Episode
 
-@receiver(post_save, sender=Course) 
+
+@receiver(post_save, sender=Course)
 def create_course_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(
-            title = f"New course: {instance.title}",
-            body = f"Hey, there's a new course: {instance.title} ({instance.video_duration}s)",
-            content_type = "Course",
-            course=instance)
+            title=f"New course: {instance.title[:100] + '...' if len(instance.title) > 100 else instance.title}",
+            body=f"Hey, there's a new course: {instance.title} ({instance.video_duration}s)",
+            content_type="Course",
+            course=instance,
+        )
 
-@receiver(post_save, sender=Episode) 
+
+@receiver(post_save, sender=Episode)
 def create_episode_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(
-            title = f"New episode: {instance.title}",
-            body = f"Hey, there's a new episode: {instance.title} (in {instance.course.title} course)",
-            content_type = "Episode",
-            episode=instance)
+            title=f"New episode: {instance.title[:100] + '...' if len(instance.title) > 100 else instance.title}",
+            body=f"Hey, there's a new episode: {instance.title} (in {instance.course.title} course)",
+            content_type="Episode",
+            episode=instance,
+        )
 
-@receiver(post_save, sender=Article) 
+
+@receiver(post_save, sender=Article)
 def create_article_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(
-            title = f"New article: {instance.title}",
-            body = f"Hey, there's a new article: {instance.title}",
-            content_type = "Article",
-            article=instance)
+            title=f"New article: {instance.title[:100] + '...' if len(instance.title) > 100 else instance.title}",
+            body=f"Hey, there's a new article: {instance.title}",
+            content_type="Article",
+            article=instance,
+        )
