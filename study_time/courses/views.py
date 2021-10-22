a3e47c002e0627812fea609fd2fddaf5c0928cf1
natalia-sk <nataliaszokaluk@gmail.com>
2021-10-22 08:40:37 +0200

    Add drf-extensions to app

--- study_time/courses/views.py
+++ study_time/courses/views.py
@@ -1,16 +1,16 @@
 from rest_framework import viewsets
+from rest_framework_extensions.mixins import NestedViewSetMixin
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Course, Episode
 from .serializers import CourseSerializer, EpisodeSerializer
 
 
-class EpisodeViewSet(viewsets.ModelViewSet):
+class EpisodeViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     serializer_class = EpisodeSerializer
-    queryset = Episode.objects.all()
+    queryset = Episode.objects.all()  #get_queryset
 
-
-class CourseViewSet(viewsets.ModelViewSet):
+class CourseViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
     filter_backends = [DjangoFilterBackend]
