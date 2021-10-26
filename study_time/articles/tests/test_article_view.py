04ce92d47b587d25228ebc1256e8fba8ad7a9c5a
natalia-sk <nataliaszokaluk@gmail.com>
2021-10-26 15:32:00 +0200

    Update 'test_articles_view.py'

--- study_time/articles/tests/test_article_view.py
+++ study_time/articles/tests/test_article_view.py
@@ -1,57 +1,83 @@
 import pytest
 
 from rest_framework import status
-from rest_framework.test import APIClient
 from rest_framework.permissions import IsAuthenticated
 
 from articles.models import Article
-from articles.serializers import ArticleSerializer
 from articles.tests import values
 from articles.views import ArticleViewSet
+
 from tests.utils import get_client
-
-
-def test_article_view_serialzier_class():
-    assert ArticleViewSet.serializer_class == ArticleSerializer
-
-
-def test_article_view_permission_classes():
-    assert ArticleViewSet.permission_classes == [IsAuthenticated]
 
 
 @pytest.mark.django_db
 def test_article_detail(user_fixture, article_fixture):
-
     # GIVEN
-    article = Article.objects.get()
-    client = get_client(user_fixture)
+    client = get_client(user_fixture)  # authenticated user
 
     # WHEN
     request = client.get(values.ARTICLE_DETAIL_PATH)
 
     # THEN
+    expected_data = {
+        "id": values.ARTICLE_ID,
+        "title": values.ARTICLE_TITLE,
+        "content": values.ARTICLE_CONTENT,
+    }
+
     assert request.status_code == status.HTTP_200_OK
-    assert request.data == {"id": article.id, "title": article.title, "content": article.content}
+    assert request.data == expected_data
+
+
+@pytest.mark.parametrize(
+    argnames=["action", "expected_permission"],
+    argvalues=[
+        pytest.param("list", [IsAuthenticated], id="list"),
+        pytest.param("create", [IsAuthenticated], id="create"),
+        pytest.param("retrieve", [IsAuthenticated], id="retrieve"),
+        pytest.param("update", [IsAuthenticated], id="update"),
+        pytest.param("partial_update", [IsAuthenticated], id="partial-update"),
+        pytest.param("destroy", [IsAuthenticated], id="destroy"),
+    ],
+)
+def test_course_view_permission_classes(action, expected_permission):
+    # GIVEN
+    article_viewset = ArticleViewSet()
+    article_viewset.action = action
+    permissions = [type(permission) for permission in article_viewset.get_permissions()]
+
+    # THEN
+    assert permissions == expected_permission
 
 
 @pytest.mark.django_db
-def test_unauthenticated_user_cannot_access_articles_view(user_fixture):
-
+@pytest.mark.parametrize(
+    argnames=["url", "http_method", "expected_status_code"],
+    argvalues=[
+        pytest.param(values.ARTICLES_LIST_PATH, "get", status.HTTP_403_FORBIDDEN, id="list"),
+        pytest.param(values.ARTICLE_DETAIL_PATH, "get", status.HTTP_403_FORBIDDEN, id="detail-view"),
+        pytest.param(values.ARTICLES_LIST_PATH, "post", status.HTTP_403_FORBIDDEN, id="create"),
+        pytest.param(values.ARTICLE_DETAIL_PATH, "put", status.HTTP_403_FORBIDDEN, id="update"),
+        pytest.param(values.ARTICLE_DETAIL_PATH, "patch", status.HTTP_403_FORBIDDEN, id="partial-update"),
+        pytest.param(values.ARTICLE_DETAIL_PATH, "delete", status.HTTP_403_FORBIDDEN, id="delete"),
+    ],
+)
+def test_article_unauthenticated_user_accesses(url, http_method, expected_status_code):
     # GIVEN
-    client = APIClient(user_fixture)
+    client = get_client()  # unauthenticated user
 
     # WHEN
-    request = client.get(values.ARTICLES_LIST_PATH)
+    request_function = getattr(client, http_method)
+    response = request_function(url)
 
     # THEN
-    assert request.status_code == status.HTTP_403_FORBIDDEN
+    assert response.status_code == expected_status_code
 
 
 @pytest.mark.django_db
 def test_create_article(user_fixture):
-
     # GIVEN
-    client = get_client(user_fixture)
+    client = get_client(user_fixture)  # authenticated user
     data = values.DATA_NEW_ARTICLE
 
     # WHEN
@@ -64,9 +90,8 @@ def test_create_article(user_fixture):
 
 @pytest.mark.django_db
 def test_list_articles(user_fixture, three_articles_fixture):
-
     # GIVEN
-    client = get_client(user_fixture)
+    client = get_client(user_fixture)  # authenticated user
 
     # WHEN
     response = client.get(values.ARTICLES_LIST_PATH)
@@ -78,17 +103,12 @@ def test_list_articles(user_fixture, three_articles_fixture):
 
 @pytest.mark.django_db
 def test_update_articles(user_fixture, article_fixture):
-
     # GIVEN
-    client = get_client(user_fixture)
+    client = get_client(user_fixture)  # authenticated user
     new_data = values.DATA_CHANGED_ARTICLE
 
     # WHEN
-    response = client.put(
-        path=values.ARTICLE_DETAIL_PATH, 
-        data=new_data, 
-        format="json"
-        )
+    response = client.put(path=values.ARTICLE_DETAIL_PATH, data=new_data, format="json")
 
     # THEN
     assert response.status_code == status.HTTP_200_OK
@@ -97,9 +117,8 @@ def test_update_articles(user_fixture, article_fixture):
 
 @pytest.mark.django_db
 def test_delete_articles(user_fixture, article_fixture):
-
     # GIVEN
-    client = get_client(user_fixture)
+    client = get_client(user_fixture)  # authenticated user
 
     # WHEN
     response = client.delete(path=values.ARTICLE_DETAIL_PATH)
