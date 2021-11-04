c8c7e0e33394d62404e1141f51900f3acb75d68b
natalia-sk <nataliaszokaluk@gmail.com>
2021-11-04 16:02:17 +0100

    Update 'values.py' files in articles and courses tests

--- study_time/articles/tests/values.py
+++ study_time/articles/tests/values.py
@@ -2,8 +2,9 @@ from django.urls import reverse
 
 
 ARTICLE_ID = 1
+THREE_ARTICLES_IDS = [2, 3]
+ARTICLES_IDS = [ARTICLE_ID] + THREE_ARTICLES_IDS
 
-ARTICLES_IDS = [1, 2, 3]
 ARTICLE_TITLE = "Some title, only for tests."
 ARTICLE_CONTENT = "A little longer text, only for tests"
 
@@ -12,23 +13,21 @@ ARTICLES_LIST_PATH = reverse("articles-list")
 
 ARTICLES_RESPONSE = {
     "count": 3,
-    "next": None, 
-    "previous": None, 
+    "next": None,
+    "previous": None,
     "results": [
-        {"id": article_id, "title": ARTICLE_TITLE, "content": ARTICLE_CONTENT} for article_id in ARTICLES_IDS
-        ]
-    }
+        {"id": article_id, "title": ARTICLE_TITLE, "content": ARTICLE_CONTENT}
+        for article_id in ARTICLES_IDS
+    ],
+}
 
-DATA_NEW_ARTICLE = {
-    "title": "test article title", 
-    "content": "test article content"
-    }
+DATA_NEW_ARTICLE = {"title": "test article title", "content": "test article content"}
 
 NEW_ARTICLE_RESPONSE = {"id": ARTICLE_ID, **DATA_NEW_ARTICLE}
 
 DATA_CHANGED_ARTICLE = {
-    "title": "test new article title", 
-    "content": "test new article content"
-    }
+    "title": "test new article title",
+    "content": "test new article content",
+}
 
 ARTICLE_UPDATE_RESPONSE = {"id": ARTICLE_ID, **DATA_CHANGED_ARTICLE}
--- study_time/courses/tests/values.py
+++ study_time/courses/tests/values.py
@@ -1,9 +1,10 @@
 from django.urls import reverse
 
+
 # COURSES
 COURSE_ID = 1
-
-COURSES_IDS = [1, 2, 3]
+THREE_COURSES_IDS = [2, 3]
+COURSES_IDS = [COURSE_ID] + THREE_COURSES_IDS
 
 COURSE_TITLE = "Some title, only for tests."
 COURSE_VIDEO_DURATION = 123
@@ -15,49 +16,52 @@ COURSE_DETAIL_PATH = reverse("course-detail", kwargs={"pk": COURSE_ID})
 COURSES_LIST_PATH = reverse("course-list")
 
 COURSES_RESPONSE = {
-    "count": 3, 
-    "next": None, 
-    "previous": None, 
+    "count": 3,
+    "next": None,
+    "previous": None,
     "results": [
-        {"url": f"http://testserver/courses/courses/{course_id}",
-        "id": course_id,
-        "title": COURSE_TITLE,
-        "video_duration": COURSE_VIDEO_DURATION,
-        "language": COURSE_LANGUAGE,
-        "episodes": []} for course_id in COURSES_IDS
-    ]
+        {
+            "url": f"http://testserver/courses/courses/{course_id}",
+            "id": course_id,
+            "title": COURSE_TITLE,
+            "video_duration": COURSE_VIDEO_DURATION,
+            "language": COURSE_LANGUAGE,
+            "episodes": [],
+        }
+        for course_id in COURSES_IDS
+    ],
 }
 
 DATA_NEW_COURSE = {
-    "title": "test course title", 
-    "video_duration": 123, 
-    "language": "fr"
-    }
+    "title": "test course title",
+    "video_duration": 123,
+    "language": "fr",
+}
 
 NEW_COURSE_RESPONSE = {
-    "id": COURSE_ID, 
-    "url": COURSE_TESTSERVER_URL, 
-    "episodes": [], 
-    **DATA_NEW_COURSE
-    }
+    "id": COURSE_ID,
+    "url": COURSE_TESTSERVER_URL,
+    "episodes": [],
+    **DATA_NEW_COURSE,
+}
 
 DATA_CHANGED_COURSE = {
-    "title": "test new course title", 
+    "title": "test new course title",
     "video_duration": 321,
-    "language": "ru"
-    }
+    "language": "ru",
+}
 
 COURSE_UPDATE_RESPONSE = {
     "id": COURSE_ID,
     "url": COURSE_TESTSERVER_URL,
-    "episodes": [], 
-    **DATA_CHANGED_COURSE
-    }
+    "episodes": [],
+    **DATA_CHANGED_COURSE,
+}
 
 # EPISODES
 EPISODE_ID = 1
-
-EPISODES_IDS = [1, 2, 3]
+THREE_EPISODES_IDS = [2, 3]
+EPISODES_IDS = [EPISODE_ID] + THREE_EPISODES_IDS
 
 EPISODE_TITLE = "Some title, only for tests."
 EPISODE_VIDEO_URL = "http://test-episode-url.com"
@@ -68,38 +72,41 @@ EPISODE_DETAIL_PATH = reverse("episode-detail", kwargs={"pk": EPISODE_ID})
 EPISODES_LIST_PATH = reverse("episode-list")
 
 EPISODES_RESPONSE = {
-    "count": 3, 
-    "next": None, 
-    "previous": None, 
+    "count": 3,
+    "next": None,
+    "previous": None,
     "results": [
-        {"url": f"http://testserver/courses/episodes/{episode_id}",
-        "id": episode_id,
-        "title": EPISODE_TITLE,
-        "video_url": EPISODE_VIDEO_URL,
-        "course": COURSE_ID} for episode_id in EPISODES_IDS
-    ]
+        {
+            "url": f"http://testserver/courses/episodes/{episode_id}",
+            "id": episode_id,
+            "title": EPISODE_TITLE,
+            "video_url": EPISODE_VIDEO_URL,
+            "course": COURSE_ID,
+        }
+        for episode_id in EPISODES_IDS
+    ],
 }
 
 DATA_NEW_EPISODE = {
     "title": "test episode title",
     "video_url": "http://test-episode.com",
-    "course": COURSE_ID
+    "course": COURSE_ID,
 }
 
 NEW_EPISODE_RESPONSE = {
     "id": EPISODE_ID,
     "url": EPISODE_TESTSERVER_URL,
-    **DATA_NEW_EPISODE
+    **DATA_NEW_EPISODE,
 }
 
 DATA_CHANGED_EPISODE = {
     "title": "test new episode title",
     "video_url": "http://test-brand-new-episode-url.com",
-    "course": COURSE_ID + 1
+    "course": COURSE_ID + 1,
 }
 
 EPISODE_UPDATE_RESPONSE = {
     "id": EPISODE_ID,
     "url": EPISODE_TESTSERVER_URL,
-    **DATA_CHANGED_EPISODE
+    **DATA_CHANGED_EPISODE,
 }
