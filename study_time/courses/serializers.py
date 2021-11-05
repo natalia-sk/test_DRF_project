9890e31510f21d5505d90ad8719aeb41de5d2b84
natalia-sk <nataliaszokaluk@gmail.com>
2021-11-05 12:17:52 +0100

    Update, after removieng 'course' field from EpisodeSerializer

--- study_time/courses/serializers.py
+++ study_time/courses/serializers.py
@@ -5,7 +5,16 @@ from .models import Course, Episode
 class EpisodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Episode
-        fields = ["id", "title", "video_url", "course"]
+        fields = ["id", "title", "video_url"]
+
+    def create(self, validated_data):
+        validated_data["course_id"] = self._get_course_id()
+        return super().create(validated_data)
+
+    def _get_course_id(self) -> int:
+        view = self.context["view"]
+        course_id = view.kwargs[view.course_id_lookup]
+        return course_id
 
 
 class CourseSerializer(serializers.ModelSerializer):
